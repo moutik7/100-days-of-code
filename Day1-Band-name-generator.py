bd55459b8dc4604c7beca5a29f7d4b6787121812
Moutik Patel <85564675+moutik7@users.noreply.github.com>
2022-10-03 21:02:05 -0600

    Day 1

---
+++ Day1-Band-name-generator.py
@@ -0,0 +1,9 @@
+# Write your code below this line 👇
+print("Welcome to Band-name Generator")
+print("Whats the name of city/town you feel its cool?")
+town_name = input()
+print("Whats the name of your pet?")
+pet_name = input()
+band_name = town_name + "ian " + pet_name
+print("We found a name for you!!!!")
+print(band_name)
